4d6ae57b3a99cede042a628e1358607e55e2235b
Fabio Carrella <fabiocarrella@gmail.com>
2020-07-20 20:58:58 +0200

    Updates

---
+++ precache-manifest.2a35ea6d7d6088518089614e67f03928.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36f2a1c44b20ca3fc45b556a9d6ea73",
+    "url": "/blog-frontend/index.html"
+  },
+  {
+    "revision": "22e846277690c7923a63",
+    "url": "/blog-frontend/static/css/main.5d65ff8a.chunk.css"
+  },
+  {
+    "revision": "26fb4fbe6e1fdc00c777",
+    "url": "/blog-frontend/static/js/2.8032e229.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/blog-frontend/static/js/2.8032e229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e846277690c7923a63",
+    "url": "/blog-frontend/static/js/main.b6eafeb5.chunk.js"
+  },
+  {
+    "revision": "689f417279d627c2501e",
+    "url": "/blog-frontend/static/js/runtime-main.40b4ade6.js"
+  },
+  {
+    "revision": "486de20a3d071eac08a0eaae5c7793fb",
+    "url": "/blog-frontend/static/media/diary.486de20a.jpg"
+  },
+  {
+    "revision": "78097e65142311c64f1052e553fc29ef",
+    "url": "/blog-frontend/static/media/hello.78097e65.jpg"
+  },
+  {
+    "revision": "50a76ad66b04ee53c9ade3ffdb262824",
+    "url": "/blog-frontend/static/media/typewriter2.50a76ad6.jpg"
+  }
+]);
